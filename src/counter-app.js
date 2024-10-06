9c43bc16825c763d7c557e9a266e0a9b70bb5acc
raen-max <rld5565@psu.edu>
2024-10-06 15:15:12 -0400

    attempt3

--- src/counter-app.js
+++ src/counter-app.js
@@ -1,77 +1,37 @@
-import { LitElement, html, css } from "lit";
-import { DDDSuper } from "@haxtheweb/d-d-d/d-d-d.js";
+import { LitElement, html } from "lit";
 
-export class counterApp extends DDDSuper(LitElement) {
-
-  static get tag() {
-    return "counter-app";
+class HandleEvents extends LitElement {
+  static get properties() {
+    return {
+      count: { type: Number }
+    };
   }
 
   constructor() {
     super();
+
     this.count = 0;
-    this.title = " ";
   }
 
-  static get properties() {
-    return {
-      title: { type: String},
-      count: {type: Number},
-    };
+  render() {
+    return html`
+      <div>
+        Current count: [${this.count}]
+        <!-- Use @[eventname] syntax to declaratively register inline event handlers -->
+        <button @click=${() => (this.count += 1)}>+</button>
+
+        <!--
+          You can also pass a function reference directly. Lit-html will automatically use the element
+          as the function scope ('this' will reference the element)
+        -->
+        <button @click=${this._onDecrement}>-</button>
+      </div>
+    `;
   }
 
-  static get styles() {
-    return [super.styles,
-    css`
-      :host {
-        display: block;
-        color: var(--ddd-theme-primary);
-        background-color: var(--ddd-theme-accent);
-        font-family: var(--ddd-font-navigation);
-        font-size: var(--counter-app-font-size, var(--ddd-font-size-s));
-      }
-      .wrapper {
-        margin: var(--ddd-spacing-2);
-        padding: var(--ddd-spacing-4);
-      }
-      div {
-        padding: 0;
-        margin: 0;
-      }
-    `];
-  }
-
-
- increment(e){
-   this.count++;
- }
-
-
- decrement(e){
-   this.count--;
- }
-
-
-
-
- render() {
-   return html`
- <div class="wrapper">
- <div>Current count: ${this.count}</div>
- <button @click="${this.decrement}">-</button>
- <button @click="${this.increment}">+</button>
- <div>${this.title}</div>
- <slot></slot>
-</div>`;
- }
-
-  /**
-   * haxProperties integration via file reference
-   */
-  static get haxProperties() {
-    return new URL(`./lib/${this.tag}.haxProperties.json`, import.meta.url)
-      .href;
+  _onDecrement() {
+    this.count -= 1;
   }
 }
 
-globalThis.customElements.define(counterApp.tag, counterApp);
+customElements.define("handle-events", HandleEvents);
